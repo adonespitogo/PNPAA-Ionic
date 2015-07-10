eec3e88ab2ab5c0b2b1b77e2c7ce0c0d29646834
Adones <pitogo.adones@gmail.com>
2015-07-11 01:34:37 +0800

    clean www folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
@@ -9,19 +10,20 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss'],
+  sass: ['app/**/*.scss'],
+  libs: [
+    'app/lib/ionic/js/ionic.bundle.js',
+  ],
   src: [
     'app/js/app.js',
     'app/js/controllers.js',
   ]
 };
 
-var src = [
-  'app/js/app.js',
-  'app/js/controllers.js',
-];
+var build = 'build.js';
+var dist = 'www/js';
 
-gulp.task('android', ['build'], function () {
+gulp.task('android', ['build', 'compress'], function () {
   return sh.exec('ionic run android');
 });
 
@@ -29,23 +31,24 @@ gulp.task('default', ['build'], function () {
   return sh.exec('ionic serve');
 });
 
-gulp.task('build', ['sass', 'lint', 'concat', 'resources']);
+gulp.task('build', ['sass', 'lint', 'concat', 'copy', 'resources']);
 
-gulp.task('concat', function (done) {
-  gulp.src(paths.src)
-  .pipe(concat('build.js'))
-  .pipe(gulp.dest('www/js/'))
-  .on('end', done);
+gulp.task('concat', function () {
+  src = paths.libs.concat(paths.src);
+  return gulp.src(src)
+  .pipe(concat(build))
+  .pipe(gulp.dest(dist));
 });
 
-gulp.task('lint', function () {
+gulp.task('lint', function (done) {
+
   return gulp.src(paths.src)
-          .pipe(jshint())
-          .pipe(jshint.reporter('default'));
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/app.scss')
+  gulp.src('./app/scss/app.scss')
     .pipe(sass({
       errLogToConsole: true
     }))
@@ -61,27 +64,30 @@ gulp.task('resources', function () {
   return sh.exec('ionic resources');
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.src, ['concat']);
-});
-
-gulp.task('install', ['git-check'], function() {
+gulp.task('install', function() {
   return bower.commands.install()
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+gulp.task('copy', ['copy-ionic-fonts', 'copy-templates']);
+
+gulp.task('copy-templates', function () {
+  return sh.exec('cp -a app/templates/. www/templates/');
+});
+
+gulp.task('copy-ionic-fonts', function () {
+  return sh.exec('cp -a app/lib/ionic/fonts/. www/fonts/ionicons/');
+});
+
+gulp.task('compress', function() {
+  return gulp.src(dist+'/'+build)
+        .pipe(uglify())
+        .pipe(gulp.dest(dist));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.src, ['concat']);
 });
