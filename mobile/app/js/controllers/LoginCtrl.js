795dde908aa12a82ee5f91538283b73040e86b83
Adones <pitogo.adones@gmail.com>
2015-07-14 03:38:20 +0800

    loging in ..

--- mobile/app/js/controllers/LoginCtrl.js
+++ mobile/app/js/controllers/LoginCtrl.js
@@ -6,16 +6,24 @@
     'Auth',
     '$state',
     '$ionicPopup',
-    function ($scope, Auth, $state, $ionicPopup) {
+    '$ionicLoading',
+    function ($scope, Auth, $state, $ionicPopup, $ionicLoading) {
 
       $scope.loginData = {};
 
       $scope.login = function () {
+
+        $ionicLoading.show({
+          template: 'Logging in...'
+        });
+
         Auth.login($scope.loginData)
         .success(function () {
           $state.go('app.discussions');
+          $ionicLoading.hide();
         })
         .error(function (resp) {
+          $ionicLoading.hide();
 
           var err = resp.err || resp;
           err = err.charAt(0).toUpperCase() + err.slice(1) + '.'; // capitalize 1st letter
