f4957a1b1cd68b8edcb87a9d3d23f64ff7984745
Adones <pitogo.adones@gmail.com>
2015-07-15 19:12:29 +0800

    add new tasks. todo: clean up tasks

--- mobile/tasks/watch.js
+++ mobile/tasks/watch.js
@@ -4,7 +4,7 @@ var assets = require("../assets.json");
 gulp.task('watch', function() {
 
 
-  gulp.watch(assets.sass.all, ['sass']);
+  gulp.watch(assets.sass, ['css']);
   gulp.watch(assets.js.src, ['lint', 'concat']);
   gulp.watch('.../assets.json', ['build:assets']);
   gulp.watch('app/**/*.html', ['copy']);
