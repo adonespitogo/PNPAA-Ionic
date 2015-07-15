bc5397f128b9e2fe09da1b4bc1372a983ec2fe03
Adones <pitogo.adones@gmail.com>
2015-07-16 02:11:21 +0800

    return gulp unzip

--- tasks/resources-unzip.js
+++ tasks/resources-unzip.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var unzip = require('gulp-unzip');
 
 gulp.task('resources:unzip', function () {
-  gulp.src("resources.zip")
+  return gulp.src("resources.zip")
     .pipe(unzip())
     .pipe(gulp.dest('./'))
 });
