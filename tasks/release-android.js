aa37100677998f897b60877885702f0632816cb0
Adones <pitogo.adones@gmail.com>
2015-07-16 05:38:07 +0800

    specify release apk

--- tasks/release-android.js
+++ tasks/release-android.js
@@ -5,19 +5,20 @@ var del = require('del');
 var path = require('path');
 var cordovaLib = require('cordova-lib').cordova;
 
+var releaseAPK = process.env.apk === 'x86'? 'android-x86-release.apk' : 'android-armv7-release.apk'
 
 var projectRoot = cordovaLib.findProjectRoot();
 
 
 gulp.task('release:android', ['build:assets','compress', 'host'], function(done) {
 
-    del(['releases/android/*', 'platforms/android/build/outputs/apk/'], function () {
+    del(['platforms/android/build/outputs/apk/'], function () {
 
       gulp.src('../')
       .pipe(android({
         storeFile: path.join(projectRoot, 'pnpaa-release.keystore'),
         keyAlias: 'alias_name',
-        releaseApk: 'android-armv7-release.apk'
+        releaseApk: releaseAPK
       }))
       .pipe(gulp.dest('releases/android/'))
       .on('end', done);
