ec47e7996167b4db95a6a206c60e92c513ace450
Adones <pitogo.adones@gmail.com>
2015-07-17 00:31:49 +0800

    update task plugins

--- tasks/plugins.js
+++ tasks/plugins.js
@@ -1,3 +1,7 @@
+// reinstall cordova plugins
+// reinstall android platform
+// reinstall crosswalk browser
+
 var gulp = require('gulp'),
     plugin = require('gulp-cordova-plugin'),
     cordovaLib = require('cordova-lib').cordova,
@@ -5,13 +9,22 @@ var gulp = require('gulp'),
     path = require('path'),
     pkg = require(path.join(projectRoot, 'package.json')),
     del = require('del'),
-    pluginsPath = path.join(projectRoot, 'plugins');
+    pluginsPath = path.join(projectRoot, 'plugins'),
+    shell = require('shelljs');
+
+gulp.task('platform:remove:android', function () {
+    return shell.exec('ionic platform rm android');
+});
 
 gulp.task('plugins:empty', function (done) {
     del(pluginsPath, done);
 });
 
-gulp.task('plugins', ['plugins:empty'], function() {
+gulp.task('add:crosswalk:browser', ['platform:remove:android', 'plugins:empty'], function () {
+    return shell.exec('ionic browser add crosswalk');
+});
+
+gulp.task('plugins', ['plugins:empty', 'platform:remove:android', 'add:crosswalk:browser'], function() {
      return gulp.src(projectRoot)
         .pipe(plugin(pkg.cordovaPlugins));
 });
